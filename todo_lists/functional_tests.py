e6b93b4d68e998ef9dcf179a34d3498c1f094406
yeguacelestial <yeguacelestial@gmail.com>
2021-01-04 16:48:03 -0600

    [+] P1-CH2:Use unittest for structuring functional test

--- todo_lists/functional_tests.py
+++ todo_lists/functional_tests.py
@@ -1,29 +1,56 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
 
-# Edith has heard about a cool new online to-do app. She goes to check out its homepage.
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# She notices the page title and header mention to-do lists
-assert 'To-Do' in browser.title
+    def setUp(self):
+        """
+            - Special method. This runs before each test.
+        """
+        self.browser = webdriver.Firefox()
 
-# She is invited to enter a to-do item straight away
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby is trying fly-fishing lures)
+    def tearDown(self):
+        """
+            - Special method. This runs AFTER each test, even if it fails.
+        """
+        self.browser.quit()
 
-# When she hits enter, the page updates, and now the page lists:
-# "1: Buy peacock feathers" as an item in a to-do list
 
-# There is still a text box inviting her to add another item. She enters "Use peacock feathers to
-# make a fly" (Edith is very methodical)
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """
+            - Test are organised into classes, which inherit from unittest.TestCase
+            - Nice descriptive names for test methods are a good idea.
+        """
+        # Edith has heard about a cool new online to-do app. She goes to check out its homepage.
+        self.browser.get('http://localhost:8000')
 
-# The page updates again, and now shows both items on her list
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
 
-# Edith wonders whether the site will remember her list. Then she sees that the site has
-# generated a unique URL for her -- there is some explanatory text to that effect.
+        # 'instance.fail' just fails no matter what, producing the given error message.
+        self.fail('Finish the test!')
 
-# She visits that URL - her to-do list is still there.
+        # She is invited to enter a to-do item straight away
 
-# Satisified, she goes back to sleep.
-browser.quit()
+        # She types "Buy peacock feathers" into a text box (Edith's hobby is trying fly-fishing lures)
+
+        # When she hits enter, the page updates, and now the page lists:
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item. She enters "Use peacock feathers to
+        # make a fly" (Edith is very methodical)
+
+        # The page updates again, and now shows both items on her list
+
+        # Edith wonders whether the site will remember her list. Then she sees that the site has
+        # generated a unique URL for her -- there is some explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisified, she goes back to sleep.
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
