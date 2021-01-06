7f77579f8e08a7c4f69e2cfc7d58e93a11fb3cd0
yeguacelestial <yeguacelestial@gmail.com>
2021-01-06 16:37:38 -0600

    Remove unnecessary line on functional tests

--- todo_lists/functional_tests.py
+++ todo_lists/functional_tests.py
@@ -35,7 +35,6 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
